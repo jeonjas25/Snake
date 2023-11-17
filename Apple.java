a5cbf9ffd69c5d34564d1381df7dfec124063207
jeonjas25 <139593914+jeonjas25@users.noreply.github.com>
2023-11-17 08:19:55 -0800

    Add files via upload
    updated

---
+++ Apple.java
@@ -0,0 +1,21 @@
+public class Apple {
+    private int x;
+    private int y;
+
+    public Apple(int maxX, int maxY) {
+       
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    // generating random coordinates within the specified coordinates
+    public void spawn(int maxX, int maxY) {
+        
+    }
+}
