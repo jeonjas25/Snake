e60b7d33f275029e10f22a74862e087319960803
Jason Jeong <139593914+jeonjas25@users.noreply.github.com>
2024-01-20 21:22:16 -0800

    Delete SnakeGame.java

--- SnakeGame.java
+++
@@ -1,7 +0,0 @@
-// SnakeGame class has the main() method
-public class SnakeGame {
-    public static void main(String[] args) {
-        GameInterface gameInterface = new GameInterface();
-    }
-    
-}
